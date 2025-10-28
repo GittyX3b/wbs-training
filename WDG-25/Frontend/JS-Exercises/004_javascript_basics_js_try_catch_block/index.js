e3f686d217489e7234b556bdd043495bac5ece91
Dandy Nagel <github@dandynagel.com>
2025-10-28 11:41:44 +0100

    004 -done

---
+++ WDG-25/Frontend/JS-Exercises/004_javascript_basics_js_try_catch_block/index.js
@@ -0,0 +1,8 @@
+try {
+  // Code that may throw an error
+  let result = someFunctionThatMightFail();
+  console.log(result);
+} catch (error) {
+  // Code to run if an error occurs
+  console.error(error.message);
+}
