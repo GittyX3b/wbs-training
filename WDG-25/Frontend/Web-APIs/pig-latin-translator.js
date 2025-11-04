c071b7bf714fa6bb86b1c535252c27072d808238
Dandy Nagel <github@dandynagel.com>
2025-11-04 21:09:27 +0100

    node-game/pig-latin - MVP final

--- WDG-25/Frontend/Web-APIs/pig-latin-translator.js
+++ WDG-25/Frontend/Web-APIs/pig-latin-translator.js
@@ -11,8 +11,102 @@ PIG LATIN RULES:
    - a vowel -> "way" is postfixed
    
 REQUIREMENTS:
-[ ] program should translate englisch text to pig latin
-[ ] program should the english text as input from process.argv
-[ ] Output the translation to the console
+[x] program should translate englisch text to pig latin
+[x] program should get the english text as input from process.argv
+[x] Output the translation to the console
   
 */
+
+// START <===========================================================
+
+// program data
+const plt = {
+  input: process.argv[2],
+  inputValid: false,
+  result: [],
+  data: {
+    consonants: [
+      "b",
+      "c",
+      "d",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+      "ß",
+    ],
+    vowels: ["a", "e", "i", "o", "u"],
+    processedString: undefined,
+  },
+  separateWordsOfInput: function () {
+    this.data.processedString = this.input.split(" ");
+  },
+  convertWords: function () {
+    this.data.processedString.forEach((word) => {
+      // temporary characteristics cache
+      let temp = {
+        firstIsVowel: null,
+        secondIsVowel: null,
+        convertedWord: [],
+        convertedString: [],
+      };
+      // investigate starting letters
+      this.data.consonants.includes(word[0].toLowerCase())
+        ? (temp.firstIsVowel = false)
+        : (temp.firstIsVowel = true);
+      word[1] && this.data.consonants.includes(word[1].toLowerCase())
+        ? (temp.secondIsVowel = false)
+        : (temp.secondIsVowel = true);
+      // do conversion
+      switch (true) {
+        case !temp.firstIsVowel && temp.secondIsVowel:
+          temp.convertedWord = word.slice(1) + word[0].toLowerCase() + "ay";
+          this.result.push(temp.convertedWord);
+          break;
+        case !temp.firstIsVowel && !temp.secondIsVowel:
+          temp.convertedWord =
+            word.slice(2) + word.slice(0, 2).toLowerCase() + "ay";
+          this.result.push(temp.convertedWord);
+          break;
+        case temp.firstIsVowel:
+          temp.convertedWord = word.toLowerCase() + "way";
+          this.result.push(temp.convertedWord);
+          break;
+        default:
+          console.log("This might be bad...");
+          break;
+      }
+    });
+  },
+  displayResult: function () {
+    console.log(``);
+    console.log(`=== PIG-LATIN-TRANSLATOR ====`);
+    console.log(`${plt.input}`);
+    console.log(`=> ${plt.result.join(" ")}`);
+  },
+};
+
+// check input
+plt.input ? (plt.inputValid = true) : console.log("input is missing");
+
+// program schedule
+if (plt.inputValid) {
+  plt.separateWordsOfInput();
+  plt.convertWords();
+  plt.displayResult();
+  process.exit(0);
+}
