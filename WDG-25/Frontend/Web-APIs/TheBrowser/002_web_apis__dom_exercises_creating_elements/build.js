3ef868503eeed8f5591416cdb5387ad9b5240c7b
Dandy Nagel <github@dandynagel.com>
2025-11-06 18:39:01 +0100

    002e js creating elements done

--- WDG-25/Frontend/Web-APIs/TheBrowser/002_web_apis__dom_exercises_creating_elements/build.js
+++ WDG-25/Frontend/Web-APIs/TheBrowser/002_web_apis__dom_exercises_creating_elements/build.js
@@ -1,19 +1,16 @@
-/* SITE CREATION */
-// creating header > nav div > ul > 4 li
-header.append(nav);
-nav.classList.add("nav");
-nav.append(navList);
-navList.classList.add("nav-list");
-navList.append(...DOM.createListItem(CONFIG.HEAD_NAV.length, ["nav-item"])); // ...(Spread-Operator) separates elemnents of returned HTMLCollection
+/* BUILD SCHEDULE =================== */
+BUILD.header();
+BUILD.heroSection();
+BUILD.footer();
 
-// creating text anchor in li elements
-[...navList.children].forEach((item, i) => {
-  let conf = CONFIG.HEAD_NAV[i];
-  let link = DOM.createTextLink(conf.text, conf.url);
-  item.append(link);
-});
+STYLE.header();
+STYLE.heroSection();
+STYLE.footer();
 
-// TODO: create the hero section
+DOM.emptyElement("body");
 
-document.body.prepend(header);
-console.log(header);
+RENDER.header();
+RENDER.heroSection();
+RENDER.footer();
+
+DOM.events.hero.button();
--- WDG-25/Frontend/Web-APIs/TheBrowser/002_web_apis__dom_exercises_creating_elements/config.js
+++ WDG-25/Frontend/Web-APIs/TheBrowser/002_web_apis__dom_exercises_creating_elements/config.js
@@ -1,9 +1,23 @@
 const CONFIG = {
-  // LINKS IN HEADER NAVIGATION
-  HEAD_NAV: [
-    { text: "Home", url: "./home.html" },
-    { text: "Menu", url: "./menu.html" },
-    { text: "About", url: "./about.html" },
-    { text: "Contact", url: "./contact.html" },
-  ],
+  HEADER: {
+    LOGO: {
+      text: "Coffee Shop",
+    },
+    NAVIGATION: [
+      { text: "Home", url: "./home.html" },
+      { text: "Menu", url: "./menu.html" },
+      { text: "About", url: "./about.html" },
+      { text: "Contact", url: "./contact.html" },
+    ],
+  },
+  HERO: {
+    HEADING: "Welcome to Our Coffee Shop",
+    TEXT: "Enjoy the best coffee in town.",
+    BUTTON: {
+      text: "Explore Our Menu",
+    },
+  },
+  FOOTER: {
+    TEXT: "© 2024 Coffee Shop. All rights reserved.",
+  },
 };
