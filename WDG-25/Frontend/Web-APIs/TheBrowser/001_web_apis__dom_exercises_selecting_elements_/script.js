563a5077ff21330bd76f08f408cb0f421e9ea550
Dandy Nagel <github@dandynagel.com>
2025-11-07 15:52:01 +0100

    001e dom selecting elements redone

--- WDG-25/Frontend/Web-APIs/TheBrowser/001_web_apis__dom_exercises_selecting_elements_/script.js
+++ WDG-25/Frontend/Web-APIs/TheBrowser/001_web_apis__dom_exercises_selecting_elements_/script.js
@@ -1,23 +1,22 @@
 // You can work here or download the template
-const h1 = document.querySelector(`h1`);
-console.log(h1);
-const aTags = document.querySelectorAll(`a`);
-console.log(aTags);
-const btn = document.querySelector(`.btn`);
-console.log(btn);
-
 const header = document.querySelector(`.header`);
+const navLinks = document.querySelectorAll(`.nav-list a`);
+const heroContent = document.querySelector(`.hero-content`);
+const heroHeading = document.querySelector(`.hero-content h1`);
+const heroButton = document.querySelector(`.btn`);
+
+console.log(heroHeading);
+console.log(navLinks);
+console.log(heroButton);
+
 header.style.background = "#b5651d";
 
-const heroContent = document.querySelector(".hero-content");
-heroContent.children[0].style.fontSize = "3rem";
+heroHeading.style.fontSize = "3rem";
 
-const navList = document.querySelector(`.nav-list`);
-const navLinks = navList.querySelectorAll(`a`);
 navLinks.forEach((link) => {
   link.style.color = "#faf0e6";
 });
 
-const newPara = document.createElement("p");
-newPara.textContent = "Open daily from 7 AM to 9 PM.";
-heroContent.appendChild(newPara);
+const heroText = document.createElement("p");
+heroText.textContent = "Open daily from 7 AM to 9 PM.";
+heroContent.append(heroText);
