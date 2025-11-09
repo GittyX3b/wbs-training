aac7f2ebe78f06886f0bbebafa11eee0cd571526
Dandy Nagel <94107254+GittyX3b@users.noreply.github.com>
2025-11-09 17:04:44 +0100

    web-apis/thebrowser - pokemon page working (#50)

--- WDG-25/Frontend/Web-APIs/TheBrowser/002_web_apis__fetch_exercises_get_data_and_add_to_dom_ii/script.js
+++ WDG-25/Frontend/Web-APIs/TheBrowser/002_web_apis__fetch_exercises_get_data_and_add_to_dom_ii/script.js
@@ -1,5 +1,9 @@
 const DOM = {
+  header: {
+    counter: document.getElementById("header-counter"),
+  },
   controller: {
+    container: document.getElementById("card-control-form"),
     offset: {
       buttonUp: document.getElementById("offset-counter-up"),
       buttonDwn: document.getElementById("offset-counter-dwn"),
@@ -22,46 +26,165 @@ const DATA = {
   pointer: {
     offset: 0,
     limit: 150,
-    limtMax: undefined,
+    limitMax: undefined,
   },
-  fetchAsync: async function () {
-    let tailoredURL = `${this.url}?offset=${this.pointer.offset}&limit=${this.pointer.limit}`;
+  fetchData: async function () {
+    try {
+      let tailoredURL = `${this.url}?offset=${this.pointer.offset}&limit=${this.pointer.limit}`;
 
-    const res = await fetch(tailoredURL);
-    this.list = await res.json();
-    this.pointer.limitMax = this.list.count;
-  },
-  controller: {
-    init: function () {},
+      const res = await fetch(tailoredURL);
+      if (!res.ok) throw new error(`list data not found - ${res.status}`);
+      const list = await res.json();
+      const results = list.results;
+
+      const listPromises = results.map(async function (item) {
+        const pokeRes = await fetch(item.url);
+        if (!pokeRes.ok)
+          throw new error(`item data [${item.name}] not found - ${res.status}`);
+        const pokeData = await pokeRes.json();
+        item.data = pokeData;
+        return item;
+      });
+
+      this.list = await Promise.all(listPromises);
+
+      this.pointer.limitMax = list.count;
+    } catch (error) {
+      alert(error);
+    }
   },
 };
 
+// TODO: match card numbers to controller clicks
 const VIEW = {
+  header: {
+    refreshCounter: () =>
+      (DOM.header.counter.textContent = DATA.pointer.limitMax),
+  },
   controller: {
-    refresh: function () {
+    init: function () {
       DOM.controller.offset.value.textContent = DATA.pointer.offset;
       DOM.controller.limit.value.textContent = DATA.pointer.limit;
     },
+    buttons: {
+      on: function () {
+        DOM.controller.offset.buttonUp.addEventListener(
+          "mouseup",
+          this.init.offset.btnUp
+        );
+        DOM.controller.offset.buttonDwn.addEventListener(
+          "mouseup",
+          this.init.offset.btnDwn
+        );
+        DOM.controller.limit.buttonUp.addEventListener(
+          "mouseup",
+          this.init.limit.btnUp
+        );
+        DOM.controller.limit.buttonDwn.addEventListener(
+          "mouseup",
+          this.init.limit.btnDwn
+        );
+        DOM.controller.container.classList.remove("bg-red-200");
+      },
+      off: function () {
+        DOM.controller.offset.buttonUp.removeEventListener(
+          "mouseup",
+          this.init.offset.btnUp
+        );
+        DOM.controller.offset.buttonDwn.removeEventListener(
+          "mouseup",
+          this.init.offset.btnDwn
+        );
+        DOM.controller.limit.buttonUp.removeEventListener(
+          "mouseup",
+          this.init.limit.btnUp
+        );
+        DOM.controller.limit.buttonDwn.removeEventListener(
+          "mouseup",
+          this.init.limit.btnDwn
+        );
+        DOM.controller.container.classList.add("bg-red-200");
+      },
+      init: {
+        offset: {
+          btnUp: function () {
+            if (DATA.pointer.offset < DATA.pointer.limitMax) {
+              DOM.controller.offset.value.textContent = ++DATA.pointer.offset;
+              VIEW.cards.refresh();
+            }
+          },
+          btnDwn: function () {
+            if (DATA.pointer.offset > 0) {
+              DOM.controller.offset.value.textContent = --DATA.pointer.offset;
+              VIEW.cards.refresh();
+            }
+          },
+        },
+        limit: {
+          btnUp: function () {
+            if (DATA.pointer.limit < DATA.pointer.limitMax - 100) {
+              DATA.pointer.limit += 10;
+              DOM.controller.limit.value.textContent = DATA.pointer.limit;
+              VIEW.cards.refresh();
+            }
+          },
+          btnDwn: function () {
+            if (DATA.pointer.limit > 1) {
+              DATA.pointer.limit -= 10;
+              DOM.controller.limit.value.textContent = DATA.pointer.limit;
+              VIEW.cards.refresh();
+            }
+          },
+        },
+      },
+    },
   },
   cards: {
-    refresh: function () {
-      DATA.list.results.map((pokemon) => {
+    refresh: async function () {
+      VIEW.controller.buttons.off();
+      DOM.cards.container.replaceChildren();
+      await DATA.fetchData();
+      DATA.list.map((pokemon) => {
         this.createCard(pokemon);
       });
+      VIEW.header.refreshCounter();
+      VIEW.controller.buttons.on();
     },
-    createCard: function ({ name, url }) {
+    createCard: function (item) {
+      let link = document.createElement("a");
       let card = document.createElement("card");
-      card.innerHTML = `<a href="${url}" target="_blank">${name}</a>`;
+      let p_img = document.createElement("img");
+      let cardText = document.createElement("div");
+      let p_name = document.createElement("h3");
+      let p_type = document.createElement("span");
+
+      link.setAttribute("href", item.url);
+      link.setAttribute("target", "_blank");
+      p_img.setAttribute(
+        "src",
+        item.data.sprites.other.dream_world.front_default
+      );
+      p_name.textContent = item.name.toUpperCase();
+      p_type.textContent = item.data.types.map((i) => i.type.name);
+
+      link.className = "flex cursor-pointer";
+      card.className =
+        "grow flex flex-col justify-around justify-between items-center h-72 p-5 rounded shadow-lg bg-yellow-200 outline-5 outline-yellow-400 hover:bg-yellow-300 hover:outline-yellow-500";
+      p_img.className = "p-7 grow max-h-48 bg-zinc-100 rounded-xl";
+      cardText.className = "flex flex-col justify-center items-center p-1";
+      p_name.className = "text-lg font-bold p-1";
+      p_type.className = "italic pb-1";
+
       // TODO: fetch and display pokemon data
-      DOM.cards.container.append(card);
+      cardText.append(p_name, p_type);
+      card.append(p_img, cardText);
+      link.append(card);
+      DOM.cards.container.append(link);
     },
   },
 };
 
 /* PROGRAMM SCHEDULE ===================================== */
-(async () => {
-  await DATA.fetchAsync(); // <-- CRITICAL: Use await here
 
-  VIEW.controller.refresh();
-  VIEW.cards.refresh();
-})();
+VIEW.cards.refresh();
+VIEW.controller.init();
