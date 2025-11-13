d0aa6cff59053ada502986a872fc3a34112cdfdf
Dandy Nagel <github@dandynagel.com>
2025-11-13 10:53:44 +0100

    js-exercises redone

--- WDG-25/2_Frontend/3_JS-Modules/1_Globals_and_more_Web-APIs/Exercises/004_web_apis__storage_exercises_more...complex_data/script.js
+++ WDG-25/2_Frontend/3_JS-Modules/1_Globals_and_more_Web-APIs/Exercises/004_web_apis__storage_exercises_more...complex_data/script.js
@@ -1,13 +1,15 @@
+const form = document.querySelector("form");
+const userInput = document.querySelector("#userInput");
+const ul = document.querySelector("ul");
+const btnReload = document.getElementById("reload");
+
 const key = "Tasks";
 
-function getStorage(k) {
-  return JSON.parse(localStorage.getItem(k));
-}
 function addToLocalStorage(k, arrEle) {
   try {
     if (!arrEle) throw new Error("arrEle argument is missing");
-    const storedArr = JSON.parse(localStorage.getItem(k));
-    const newArr = storedArr ? [arrEle, ...storedArr] : [arrEle];
+    const storedArr = JSON.parse(localStorage.getItem(k)) ?? [];
+    const newArr = [arrEle, ...storedArr];
     localStorage.setItem(k, JSON.stringify(newArr));
   } catch (error) {
     console.log(error);
@@ -15,50 +17,52 @@ function addToLocalStorage(k, arrEle) {
 }
 function deleteFromLocalStorage(k, taskId) {
   try {
-    const storedList = JSON.parse(localStorage.getItem(k));
+    const storedList = JSON.parse(localStorage.getItem(k)) ?? [];
     if (!storedList) throw new Error("no storedList found");
     const newList = storedList.filter((entry) => entry.id != taskId);
     if (!newList) throw new Error("problems getting newList");
-    localStorage.setItem(key, JSON.stringify(newList));
+    newList.length > 0
+      ? localStorage.setItem(key, JSON.stringify(newList))
+      : localStorage.removeItem(k);
   } catch (error) {
     console.log(error);
   }
 }
-function deleteTask(id) {
-  if (id) {
-    deleteFromLocalStorage(key, id);
-    refreshTaskList();
-  }
-}
 
 function refreshTaskList() {
-  let taskArr = JSON.parse(localStorage.getItem(key));
-  if (!taskArr) taskArr = [];
-  document.querySelector("ul").innerHTML = "";
-  taskArr.map((task) => {
-    const li = document.createElement("li");
-    const txt = document.createElement("div");
-    const btn = document.createElement("button");
+  let taskArr = JSON.parse(localStorage.getItem(key)) ?? [];
+  ul.innerHTML = "";
+  taskArr.map((task) => createTask(task));
+}
 
-    li.setAttribute("id", task.id);
-    txt.textContent = task.content;
-    btn.textContent = "Delete";
+function createTask(task) {
+  const li = document.createElement("li");
+  const txt = document.createElement("div");
+  const btn = document.createElement("button");
 
-    li.className =
-      "bg-green-200 rounded flex justify-between items-center px-5 py-2 mb-1 select-none";
-    txt.className = "font-bold text-sm";
-    btn.className =
-      "bg-red-500 px-2 py-1 rounded-2xl text-xs text-white cursor-pointer";
+  li.setAttribute("id", task.id);
+  txt.textContent = task.content;
+  btn.textContent = "Delete";
 
-    li.append(txt, btn);
-    document.querySelector("ul").append(li);
+  li.className =
+    "bg-green-200 rounded flex justify-between items-center px-5 py-2 mb-1 select-none";
+  txt.className = "font-bold text-sm";
+  btn.className =
+    "bg-red-500 px-2 py-1 rounded-2xl text-xs text-white cursor-pointer";
+
+  li.append(txt, btn);
+  ul.append(li);
+
+  btn.addEventListener("click", function () {
+    deleteFromLocalStorage(key, task.id);
+    refreshTaskList();
   });
 }
 
-document.querySelector("form").addEventListener("submit", function (e) {
+form.addEventListener("submit", function (e) {
   // e.preventDefault();
   try {
-    const input = document.querySelector("#userInput").value.trim();
+    const input = userInput.value.trim();
     if (!input) throw new Error("input is empty");
     let uuid = `task-${self.crypto.randomUUID().replaceAll("-", "")}`;
     const task = {
@@ -72,14 +76,5 @@ document.querySelector("form").addEventListener("submit", function (e) {
   }
 });
 
-document.querySelector("ul").addEventListener("click", function (e) {
-  if (e.target.nodeName === "BUTTON") {
-    deleteTask(e.target.parentElement.id);
-  }
-});
-
-document
-  .getElementById("reload")
-  .addEventListener("click", () => window.location.reload());
-
+btnReload.addEventListener("click", () => window.location.reload());
 window.addEventListener("load", refreshTaskList);
