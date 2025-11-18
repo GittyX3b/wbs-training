06ff55cbc042ca09d6242a53d96caab85223d7aa
Dandy Nagel <github@dandynagel.com>
2025-11-18 14:28:04 +0100

    lecture 001e react basics components  - done

---
+++ WDG-25/2_Frontend/4_React-Intro/Exercises/001_react_basics_exercises_creating_and_nesting_components/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <div>
+    <i>This is Footer</i>
+  </div>
+);
+
+export default Footer;
---
+++ WDG-25/2_Frontend/4_React-Intro/Exercises/001_react_basics_exercises_creating_and_nesting_components/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Navigation from "./Navigation";
+
+const Header = () => (
+  <div>
+    <h1>Header</h1>
+    <Navigation />
+  </div>
+);
+
+export default Header;
