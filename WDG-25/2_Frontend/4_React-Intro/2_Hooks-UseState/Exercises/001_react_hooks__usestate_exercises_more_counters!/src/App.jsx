d6a81396958db689ebd46edc9a747ec528e29ef0
Dandy Nagel <github@dandynagel.com>
2025-11-21 17:16:05 +0100

    lectures exercises redone

--- WDG-25/2_Frontend/4_React-Intro/2_Hooks-UseState/Exercises/001_react_hooks__usestate_exercises_more_counters!/src/App.jsx
+++ WDG-25/2_Frontend/4_React-Intro/2_Hooks-UseState/Exercises/001_react_hooks__usestate_exercises_more_counters!/src/App.jsx
@@ -5,17 +5,18 @@ const Counter = () => {
   const [counter, setCounter] = useState(0);
 
   return (
-    <>
+    <div className="counter">
       <button onClick={() => setCounter((p) => p + 1)}>+</button>
       <span>{counter}</span>
       <button onClick={() => setCounter((p) => p && p - 1)}>-</button>
-    </>
+    </div>
   );
 };
 
 const App = () => {
   return (
     <div>
+      <h1>More Counters!</h1>
       <Counter />
       <Counter />
       <Counter />
