98be37b395a1b71c792088efa8705bdbc4de8592
Dandy Nagel <github@dandynagel.com>
2026-01-08 15:45:36 +0100

    input validation with zod - incomplete

---
+++ WDG-25/3_Backend/9_RESTful_APIs/Zod/InputValidation_with_Zod/src/routes/postRouter.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import {
+  getPosts,
+  createPost,
+  getPostById,
+  updatePost,
+  deletePost,
+} from "#controllers";
+import { authMiddleWare, validateBodyZod } from "#middlewares";
+import { postInputSchema } from "#schemas";
+
+const postRouter = Router();
+
+postRouter.get("/", getPosts);
+postRouter.post(
+  "/",
+  validateBodyZod(postInputSchema),
+  authMiddleWare,
+  createPost
+);
+postRouter.get("/:id", getPostById);
+postRouter.put(
+  "/:id",
+  validateBodyZod(postInputSchema),
+  authMiddleWare,
+  updatePost
+);
+postRouter.delete("/:id", authMiddleWare, deletePost);
+
+//alternative
+// postRouter.route('/').get(getPosts).post(createPost);
+// postRouter.route('/:id').get(getPostById).put(updatePost).delete(deletePost);
+
+export default postRouter;
