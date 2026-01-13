2ecda9dca0ab3c6756a1e5d3756defcf4d9f5794
Dandy Nagel <94107254+GittyX3b@users.noreply.github.com>
2026-01-14 00:28:11 +0100

    initial setup and started AUTH /register (#117)

---
+++ WDG-25/3_Backend/10_Custom_Authentication_Workflow/WBS-Travel-App/AUTH-SERVER/src/middlewares/index.ts
@@ -0,0 +1,5 @@
+// TODO: Implement
+// - notFoundHandler.ts
+
+export * from './errorHandler.ts';
+export * from './validateHandler.ts';
---
+++ WDG-25/3_Backend/10_Custom_Authentication_Workflow/WBS-Travel-App/AUTH-SERVER/src/utils/index.ts
@@ -0,0 +1,4 @@
+/* For custom types used in more than one module */
+export * from './password.ts';
+export * from './token.ts';
+export * from './register.ts';
