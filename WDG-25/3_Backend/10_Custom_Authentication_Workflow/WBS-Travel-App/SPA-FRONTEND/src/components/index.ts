2ad17293ed9501693416df5df35d14d5159c63f2
Dandy Nagel <github@dandynagel.com>
2026-01-22 12:00:36 +0100

    removed spa travel-app to resolve merge conflict

--- WDG-25/3_Backend/10_Custom_Authentication_Workflow/WBS-Travel-App/SPA-FRONTEND/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import Navbar from './UI/Navbar';
-import PostCard from './Posts/PostCard';
-import PostSkeleton from './UI/PostSkeleton';
-import PostsSkeleton from './UI/PostsSkeleton';
-
-export { Navbar, PostCard, PostSkeleton, PostsSkeleton };
